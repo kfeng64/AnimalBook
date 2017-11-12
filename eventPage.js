5cf70adfccb386058c4a3a4985dfc8388436ca8b
Kevin Feng <kfeng64@gmail.com>
2017-11-12 05:55:05 -0500

    almost broke it

--- eventPage.js
+++ eventPage.js
@@ -4,22 +4,18 @@ window.onload = function(){
 
 function setChoice() {
 	var dropdown = document.getElementById("dropdown").value;
-	alert("test");
+
 	if (dropdown == "dogs") {
 		chrome.storage.sync.set({"choice": "dogs"}, function() {
-		alert("option saved");
 	});
 	} else if (dropdown == "cats") {
 		chrome.storage.sync.set({"choice": "cats"}, function() {
-		alert("option saved");
 	});
 	} else if (dropdown == "other") {
 		chrome.storage.sync.set({"choice": "other"}, function() {
-		alert("option saved");
 	});
 	} else if (dropdown == "all") {
 		chrome.storage.sync.set({"choice": "all"}, function() {
-		alert("option saved");
 	});
 	}
 }
--- picReplace.js
+++ picReplace.js
@@ -1,46 +1,31 @@
 window.onload = main();
 
 function main() {
-	alert("Script Running");
+	//alert("Script Running");
 
-	var animals = ["https://i.imgur.com/SBBdO8b.jpg", "https://i.imgur.com/B7UJFRn.jpg", "https://i.imgur.com/gIunJ2r.jpg", "https://i.imgur.com/5O5LdRB.jpg", "https://i.imgur.com/sNhzxmR.jpg", "https://68.media.tumblr.com/10776e624890ff1b69653e6a0b649b1e/tumblr_inline_ortrrhjE1f1qzf6t4_540.png", "https://i.imgur.com/aBZ2pmU.png"];
+	//Animal picture sets
+	var dogs = ["https://i.imgur.com/SBBdO8b.jpg", "https://i.imgur.com/B7UJFRn.jpg", "https://i.imgur.com/gIunJ2r.jpg", "https://i.imgur.com/5O5LdRB.jpg", "https://i.imgur.com/sNhzxmR.jpg", "https://68.media.tumblr.com/10776e624890ff1b69653e6a0b649b1e/tumblr_inline_ortrrhjE1f1qzf6t4_540.png", "https://i.imgur.com/aBZ2pmU.png"];
+
+	var cats = ["https://i.imgur.com/Lb72oOt.jpg", "https://i.imgur.com/a8g7LN3.jpg", "https://i.imgur.com/1eGvND1.jpg", "https://i.imgur.com/JAyZUbr.jpg", "https://i.imgur.com/aSPfMtv.jpg", "https://i.imgur.com/eMOJo5x.jpg"];
+
+	var other = ["https://i.imgur.com/6APa6U5.jpg", "https://i.imgur.com/UotvQlG.jpg", "https://i.imgur.com/qUU8tSM.jpg", "https://i.imgur.com/JsEF641.jpg", "https://i.imgur.com/n5dhNzj.jpg", "https://i.imgur.com/7w8Pn2I.jpg"];
+
 	var choice = "";
+	var animals = dogs; //Default to dogs
 	
 	chrome.storage.sync.get("choice", function(result) {
 		choice = result.choice;
-		alert(choice);
+		//alert(choice);
 		if (choice == "dogs"){
-		  	animals = ["https://i.imgur.com/SBBdO8b.jpg", "https://i.imgur.com/B7UJFRn.jpg", "https://i.imgur.com/gIunJ2r.jpg", "https://i.imgur.com/5O5LdRB.jpg", "https://i.imgur.com/sNhzxmR.jpg", "https://68.media.tumblr.com/10776e624890ff1b69653e6a0b649b1e/tumblr_inline_ortrrhjE1f1qzf6t4_540.png", "https://i.imgur.com/aBZ2pmU.png"];
+		  	animals = dogs;
 		  } else if (choice == "cats"){
-		  	animals = ["https://i.imgur.com/Lb72oOt.jpg", "https://i.imgur.com/a8g7LN3.jpg", "https://i.imgur.com/1eGvND1.jpg", "https://i.imgur.com/JAyZUbr.jpg", "https://i.imgur.com/aSPfMtv.jpg", "https://i.imgur.com/eMOJo5x.jpg"];
-		  	alert("cats");
+		  	animals = cats;
 		  } else if (choice == "other") {
-		  	animals = ["https://i.imgur.com/6APa6U5.jpg", "https://i.imgur.com/UotvQlG.jpg", "https://i.imgur.com/qUU8tSM.jpg", "https://i.imgur.com/JsEF641.jpg", "https://i.imgur.com/n5dhNzj.jpg", "https://i.imgur.com/7w8Pn2I.jpg"];
+		  	animals = other;
 		  } else if (choice == "all") {
-		  	animals = [];
+		  	animals.concat(dogs, cats, other);
 		  }
 	});
-
-	  
-
-
-
-	// var port = chrome.runtime.connect({name: "choice"});
-	// port.postMessage({listen: "Listening"});
-	// port.onMessage.addListener(function(msg) {
-	//   if (msg.choice == "dogs"){
-	//   	animals = ["https://i.imgur.com/SBBdO8b.jpg", "https://i.imgur.com/B7UJFRn.jpg", "https://i.imgur.com/gIunJ2r.jpg", "https://i.imgur.com/5O5LdRB.jpg", "https://i.imgur.com/sNhzxmR.jpg", "https://68.media.tumblr.com/10776e624890ff1b69653e6a0b649b1e/tumblr_inline_ortrrhjE1f1qzf6t4_540.png", "https://i.imgur.com/aBZ2pmU.png"];
-	//   } else if (msg.choice == "cats"){
-	//   	animals = ["https://i.imgur.com/Lb72oOt.jpg", "https://i.imgur.com/a8g7LN3.jpg", "https://i.imgur.com/1eGvND1.jpg", "https://i.imgur.com/JAyZUbr.jpg", "https://i.imgur.com/aSPfMtv.jpg", "https://i.imgur.com/eMOJo5x.jpg"];
-	//   } else if (msg.choice == "other") {
-	//   	animals = ["https://i.imgur.com/6APa6U5.jpg", "https://i.imgur.com/UotvQlG.jpg", "https://i.imgur.com/qUU8tSM.jpg", "https://i.imgur.com/JsEF641.jpg", "https://i.imgur.com/n5dhNzj.jpg", "https://i.imgur.com/7w8Pn2I.jpg"];
-	//   } else if (msg.choice == "all") {
-	//   	animals = [];
-	//   }
-	// });
-
-
-
 
 	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 		
@@ -69,9 +54,6 @@ function rndPic(animals) {
 
 
 function replacePics(animals) {
-	// var dogArray = ["https://i.imgur.com/SBBdO8b.jpg", "https://i.imgur.com/B7UJFRn.jpg", "https://i.imgur.com/gIunJ2r.jpg", "https://i.imgur.com/5O5LdRB.jpg", "https://i.imgur.com/sNhzxmR.jpg", "https://68.media.tumblr.com/10776e624890ff1b69653e6a0b649b1e/tumblr_inline_ortrrhjE1f1qzf6t4_540.png", "https://i.imgur.com/aBZ2pmU.png"];
-
-
 
 	//Replaces profile picture icon of a post
 	var postPic = document.getElementsByClassName("_s0 _4ooo _5xib _5sq7 _44ma _rw img");
@@ -80,8 +62,6 @@ function replacePics(animals) {
 		if (!animals.includes(postPic[i].src)){
 			postPic[i].src = rndPic(animals);
 		}
-		
-		
 	}
 
 	//Replaces profile picture icons of commentors
@@ -90,7 +70,6 @@ function replacePics(animals) {
 		if (!animals.includes(commentPic[i].src)){
 			commentPic[i].src = rndPic(animals);
 		}
-		
 	}
 
 	//Replaces profile picture when hovering over user
@@ -99,7 +78,6 @@ function replacePics(animals) {
 		if (!animals.includes(hoverProfPic[i].src)){
 			hoverProfPic[i].src = rndPic(animals);
 		}
-		
 	}
 
 }
